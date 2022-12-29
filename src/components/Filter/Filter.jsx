b5ce3aafc2e18bd2ca171ed48044410bfb0a2147
Maksym Kolii <maksymkolii@gmail.com>
2022-12-29 23:20:58 +0100

    commit

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import { React } from 'react';
 import { Label, Input } from './Filter.styled';
 import PropTypes from 'prop-types';
 
-export class Filter extends Component {
-  render() {
-    return (
-      <Label>
-        Find contacts by name
-        <Input
-          type="text"
-          name="filter"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={this.props.filterChange}
-          value={this.props.filtered}
-        />
-      </Label>
-    );
-  }
-}
+export const Filter = ({ filterChange, filtered }) => {
+  return (
+    <Label>
+      Find contacts by name
+      <Input
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        onChange={filterChange}
+        value={filtered}
+      />
+    </Label>
+  );
+};
 
 Filter.propTypes = {
   onChange: PropTypes.func,
