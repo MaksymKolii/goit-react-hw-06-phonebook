c100444f619a4836a104b438cc95d7ce12740cf2
Maksym Kolii <maksymkolii@gmail.com>
2023-01-01 23:53:51 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { App } from 'components/App';
 import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-console.log(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
