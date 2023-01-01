f2aa0490f2a9fb4bb36971bf70dd7c7411d244aa
Maksym Kolii <maksymkolii@gmail.com>
2023-01-01 23:44:54 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,49 +7,8 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
 import { Section } from './Section/Section';
-// import { useState } from 'react';
 
 export function App() {
-  // const [contacts, setContacts] = useLocalStorage('contacts', []);
-
-  // const [filter, setFilter] = useState('');
-
-  // // *const addContact = (name, number) => {
-  // const addContact = data => {
-  //   const newContact = {
-  //     id: nanoid(),
-  //     //* name,
-  //     //* number,
-  //     ...data,
-  //   };
-
-  //   const isNameExist = contacts.find(({ name, number }) => {
-  //     return name === newContact.name || number === newContact.number;
-  //   });
-
-  //   isNameExist
-  //     ? window.alert(`${newContact.name} is alredy in contacts!`)
-  //     : setContacts(prevS => [newContact, ...prevS]);
-  // };
-
-  // const deleteContact = iD => {
-  //   setContacts(prevContacts => prevContacts.filter(({ id }) => id !== iD));
-  // };
-
-  // const onFilterChange = ev => {
-  //   setFilter(ev.target.value);
-  // };
-
-  // const getFilteredContacts = () =>
-  //   contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(filter.toLowerCase())
-  //   );
-
-  // const showContactsOptions = () => {
-  //   return filter ? getFilteredContacts() : contacts;
-  // };
-
-  // const options = showContactsOptions();
   return (
     <>
       <Section>
