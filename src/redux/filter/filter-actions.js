1a8d458a4963acafd3b211fdc25c6244b4582e9e
Maksym Kolii <maksymkolii@gmail.com>
2023-01-03 13:19:18 +0100

    commit

--- src/redux/filter/filter-actions.js
+++
@@ -1,9 +0,0 @@
-// export const setFilter = value => {
-//   return {
-//     type: 'filters/filter',
-//     payload: value,
-//   };
-// };
-
-// import { createAction } from '@reduxjs/toolkit';
-// export const setFilter = createAction('filters/filter');
